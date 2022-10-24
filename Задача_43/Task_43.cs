c3f52dba0b4eee614e33425f1437fa446aa95f67
Artem Milokhov <omichdno@gmail.com>
2022-10-24 16:20:37 +0300

    task 43 complete

---
+++ Задача_43/Task_43.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Spectrum(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2 && b1 == b2)
+    {
+        Console.WriteLine("Линии совпадают.");
+    }
+    else if (k1 == k2)
+    {
+        Console.WriteLine("Линии параллельны.");
+    
+    }
+    else
+    {
+        double x = (-b1 + b2) / (-k1 + k2);
+        double y = k2 * x + b2;
+        x = Math.Round(x, 2);
+        y = Math.Round(y, 2);
+        Console.WriteLine($"Пересечение в точке -> ({x};{y})");
+    }
+}
+
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+
+Spectrum(b1, k1, b2, k2);
