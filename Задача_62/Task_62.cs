c9ef2239fba95ccadf9bd74b3bae9c971de61bbd
Artem Milokhov <omichdno@gmail.com>
2022-11-15 14:01:24 +0300

    task 62 complete

---
+++ Задача_62/Task_62.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+void GetSpiralArray(int[,] array, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { array[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = new int [rows, columns];
+if (rows != columns)
+{
+    Console.Write("Количество строк должно быть равным количесву столбцов. Попробуйте снова.");
+}
+else
+{
+GetSpiralArray(array, rows);
+PrintArray(array);
+}
